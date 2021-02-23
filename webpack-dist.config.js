1e45c466b5a23d491d7de351ea25b4ba87024ac4
lihw <875165241@qq.com>
2021-02-23 18:44:56 +0800

    修改简历

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -109,7 +109,7 @@ module.exports = {
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'https://634323580.github.io/resume/index.html' // 这里注意改成你的在线简历的网站
+        'http://resume.lihuiwen.cn' // 这里注意改成你的在线简历的网站
       ]);
 
       // 重新发布到 ghpages
