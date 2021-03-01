fc2c8094e5064ebe4f085fc4013908d21a52c965
lihw <875165241@qq.com>
2021-03-01 09:46:11 +0800

    修改简历v2

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -104,7 +104,7 @@ module.exports = {
       // 自定义域名
       // fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.wuhaolin.cn');
 
-      await publishGhPages();
+      //await publishGhPages();
 
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
